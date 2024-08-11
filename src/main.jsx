f983d9128bf23e7f4be1918e18f26d148412b0ce
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-08-11 14:45:01 +0300

    install redux-toolkit, add store folder with future slices and store component, connect store to main.jsx and wrapped it in provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app/App";
-import "./index.scss";
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/App';
+import './index.scss';
+import { Provider } from 'react-redux';
+import store from './store';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>,
 );
