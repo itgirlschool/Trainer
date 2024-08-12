85f7955b56bf9ba239c9594752ae258fe863e9d0
unknown <you@example.com>
2024-08-12 09:38:48 +0300

    add greetingpage, add some vars

--- src/Pages/Greeting/Greeting.jsx
+++ src/Pages/Greeting/Greeting.jsx
@@ -1,5 +1,61 @@
 import React from "react";
+import cat from "../../assets/images/cat_greeting.svg";
+import paw from "../../assets/images/paw.svg";
+import titleIcon from "../../assets/images/vector1.svg";
+import arrow from "../../assets/images/arrow_07.svg";
+import arrowRight from "../../assets/images/arrow-right.svg";
+import "./Greeting.scss";
 
 export default function Greeting() {
-  return <div>Greeting</div>;
+  return (
+    <>
+      <section className="greeting__outer">
+        <div className="content_wrapper">
+          <div className="content_title">
+            <div className="content_title_wrapper">
+              <h3 className="greeting__text_black bl1">
+                Добро пожаловать
+                <img src={titleIcon} alt="icon" />
+              </h3>
+              <p className="greeting__text_pink">
+                в наши удивительные тренажеры
+              </p>
+              <p className="greeting__text_black bl2">по HTML и CSS!</p>
+            </div>
+          </div>
+          <div className="content_body">
+            <div className="wrap_box_first">
+              {" "}
+              <div className="box box_first">
+                Мы рады приветствовать тебя в нашем виртуальном мире, где ты
+                сможешь познакомиться с HTML и CSS. Это приложение поможет тебе
+                погрузиться в веб-разработку и попробовать себя в качестве
+                разработчика.
+              </div>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="box box_second">
+              Не волнуйся, если у тебя нет предварительного опыта в
+              программировании. Наш тренажер #ITGIRLS предназначен для
+              начинающих: для тех, кто еще не определился и хотел бы
+              познакомиться с этим направлением поближе. Мы будем тебя
+              поддерживать на каждом шагу, предоставляя пошаговые инструкции и
+              полезные подсказки.
+            </div>
+          </div>
+          <div className="btn_wrapper">
+            <p>Создать профиль</p>
+            <img src={arrowRight} alt="arrow" />
+          </div>
+          <div className="content_image"></div>
+        </div>
+        <div className="images_wrapper">
+          <img src={cat} alt="cat" />
+        </div>
+      </section>
+      <div className="wrapper_paw">
+        <img src={paw} alt="paw" />
+      </div>
+    </>
+  );
 }
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,11 @@
 import "./App.scss";
+import Greeting from "../Pages/Greeting/Greeting";
 
 function App() {
-  return <>hello world</>;
+  return (
+  // <>hello world</>
+    <Greeting/>
+  );
 }
 
 export default App;
