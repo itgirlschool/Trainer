9b5037563bca5faa51ab7ec6f70daf3796f86e20
LeonovEgorF <egor1908608@gmail.com>
2024-08-21 14:56:54 +0300

    edit

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,20 +3,22 @@ import { Route, Routes } from 'react-router-dom';
 import Greeting from "../Pages/Greeting/Greeting";
 import Authorisation from '../Pages/Auth/Auth';
 import Home from '../Pages/Home/Home';
-import TaskList from '../Pages/TasksList/TaskList';
 import TaskPage from '../Pages/TaskPage/TaskPage';
 import Profile from '../Pages/Profile/Profile';
 import ErrorPage from '../Pages/Error/Error';
+import Header from '../Components/Header/Header';
+import SubjectList from "../Pages/SubjectList/SubjectList.jsx";
 
 function App() {
   return (
     <div className="app">
+      <header className="header"><Header /></header>
       <main className="main">
         <Routes>
           <Route path="/auth" element={<Authorisation />} />
           <Route path="/greeting" element={<Greeting />} />
           <Route path="/" element={<Home />} />
-          <Route path="/tasks" element={<TaskList />} />
+          <Route path="/subjectlist" element={<SubjectList />} />
           <Route path="/task" element={<TaskPage />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<ErrorPage />} />
